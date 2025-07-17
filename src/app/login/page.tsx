4ab12e30e8600bef20ea15631c553dcc0e7dcc55
Ajith <ajithpkumar231@gmail.com>
2025-07-18 04:13:34 +0530

    orders page added, FE done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Login() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      Login
+    <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
+      <div className="h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
+        <div className="relative h-1/3 w-full md:h-full md:w-1/2">
+          <Image src={"/loginBg.png"} alt="" fill className="object-cover" />
+        </div>
+        <div className="p-10 flex flex-col gap-8 md:w-1/2">
+          <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
+          <p>Log into you accpunt or create a new one using social buttons</p>
+          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+            <Image src={"/google.png"} alt="" width={20} height={20} className="object-contain" />
+            <span>Sign in with Google</span>
+          </button>
+          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+            <Image src={"/facebook.png"} alt="" width={20} height={20} className="object-contain" />
+            <span>Sign in with Facebook</span>
+          </button>
+          <p className="text-sm">Have a problem? <Link className="underline" href={"/contact"}>Contact us</Link></p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,7 +1,34 @@
 export default function OrdersPage() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      OrdersPage
+    <div className="p-4 lg:px-20 xl:px-40">
+      <table className="w-full border-separate border-spacing-3">
+        <thead>
+          <tr className="text-left">
+            <th className="hidden md:block">Order ID</th>
+            <th>Date</th>
+            <th>Price</th>
+            <th className="hidden md:block">Product</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="text-sm md:text-base odd:bg-gray-100">
+            <td className="hidden md:block py-6 px-1">545454154545445</td>
+            <td className="py-6 px-1">19.02.2024</td>
+            <td className="py-6 px-1">80.21</td>
+            <td className="hidden md:block py-6 px-1">Pizza(2)</td>
+            <td className="py-6 px-1">On the way (10 mins)</td>
+          </tr>
+          <tr className="text-sm md:text-base odd:bg-gray-100">
+            <td className="hidden md:block py-6 px-1">545454154545445</td>
+            <td className="py-6 px-1">19.02.2024</td>
+            <td className="py-6 px-1">80.21</td>
+            <td className="hidden md:block py-6 px-1">Pizza(2)</td>
+            <td className="py-6 px-1">On the way (10 mins)</td>
+          </tr>
+
+        </tbody>
+      </table>
     </div>
   );
 }
